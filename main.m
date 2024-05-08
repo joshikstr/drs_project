a336a133a5903871b7f13e3d3c788eb99193bce0
JOSHI-PC\joshu <joshua.koester100@gmail.com>
2024-05-08 13:05:53 +0200

    pca first image

--- main.m
+++ main.m
@@ -3,5 +3,26 @@ clear
 close all
 clc
 
-addpath data
-addpath functions
+addpath data\
+addpath functions\
+
+imds = imageDatastore("data\images\");
+
+%% bsp first image
+
+img_1 = readimage(imds,1);
+imshow(img_1)
+
+img_1_noise = imnoise(img_1,'gaussian');
+figure
+imshow(img_1_noise)
+
+%% pca
+
+coeff = pca(double(img_1_noise));
+figure
+idisp(coeff)
+
+coeff_ref = pca(double(img_1));
+figure
+idisp(coeff_ref)
