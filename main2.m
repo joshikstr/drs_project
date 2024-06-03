89f0b30ad3f907f703fd3237721ac729d988472f
danielbde <dabo@freenet.de>
2024-06-03 17:26:05 +0200

    PSNR überprüft

--- main2.m
+++ main2.m
@@ -65,28 +65,36 @@ figure
 imshow(imgRecons)
 
 
-%Korrelation Ursprungsbild und PCA
+%% Korrelation Ursprungsbild und PCA
 
 % Berechnung der Korrelation zwischen den beiden rekonstruierten Bildern
 correlation = corr2(imgOrig, imgRecons);
 
-fprintf('Die Korrelation zwischen den beiden optimierten Bildern beträgt: %.4f\n\n', correlation);
+fprintf('Die Korrelation zwischen dem Oridinalbild und dem PCR-Bild beträgt: %.4f\n\n', correlation);
 
 
-%Rausch Index
-% -> SNR misst die Qualität eines Signals im Allgemeinen, indem es das Verhältnis von Signalstärke zu Rauschstärke angibt.
+%% Rausch Index
+
 % -> PSNR ist spezifisch für Bilder und Videos und vergleicht die Qualität eines rekonstruierten Bildes mit dem Originalbild durch das Verhältnis von maximaler Signalstärke zur mittleren quadratischen Abweichung.
 
 %-> Ein höherer PSNR-Wert deutet auf eine höhere Qualität der rekonstruierten oder komprimierten Bilddaten hin, da der Fehler (Rauschen) im Vergleich zum Signal kleiner ist.
 
 %Rausch Index Original Bild zu Noise Bild
-[peaksnr, snr] = psnr(imgOrig, imgNoise); 
-fprintf('Peak-SNR original Image zu noise Image:  %0.4f \n', peaksnr);
-fprintf('SNR original Image zu noise Image:  %0.4f \n\n', snr);
+peaksnr = psnr(imgOrig, imgNoise); 
+fprintf('Peak-SNR original Image zu noise Image:  %0.4f dB\n', peaksnr);
 
 %Rausch Index PCR-Bild zu Noise Bild
-[peaksnr, snr] = psnr(imgRecons, imgNoise); 
-fprintf('Peak-SNR PCA Image zu noise Image:  %0.4f \n', peaksnr);
-fprintf('SNR original Image zu noise Image:  %0.4f \n\n', snr);
+peaksnr = psnr(imgRecons, imgNoise); 
+fprintf('Peak-SNR PCA Image zu noise Image:  %0.4f dB\n', peaksnr);
 
 
+%% SSIM   -> macht für uns wenig bis keinen Sinn
+
+%SSIM ist ein Maß, das die strukturelle Ähnlichkeit zwischen zwei Bildern bewertet. Es berücksichtigt Helligkeit, Kontrast und Struktur. SSIM-Werte liegen im Bereich von -1 bis 1, wobei 1 eine perfekte Übereinstimmung bedeutet.
+
+ssimval = ssim(imgOrig,imgNoise);
+fprintf('SSIM original Image zu noise Image:  %0.4f \n', ssimval);
+
+
+ssimval = ssim(imgRecons,imgNoise);
+fprintf('SSIM PCA Image zu noise Image:  %0.4f \n', ssimval);
